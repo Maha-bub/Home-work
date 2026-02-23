94ab0543c32704c2b797966dc23aea3e6e63e63e
Mahabu-bub <mdmahabubulalam0511@gmail.com>
2026-02-23 10:23:28 +0600

    loop continue and break statement

--- do-while.js
+++ do-while.js
@@ -1,13 +1,18 @@
-var a =5 ;
-do{
-    console.log("This i a number:", a);
-    a++;
-}while(a<=10);
+// var a =5 ;
+// do{
+//     console.log("This i a number:", a);
+//     a++;
+// }while(a<=10);
 
 
 
-var i =4;
-while(i<=10){
-    console.log(i);
+var i = 1;
+while (i <= 10) {
     i++;
+    if (i === 3) {
+        continue;
+    }
+    console.log(i);
+
+
 }
